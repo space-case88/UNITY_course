58b6580eb23a775c97fa08004fba19744c31ef6c
space-case88 <128758348+space-case88@users.noreply.github.com>
2023-05-26 16:22:01 -0700

    Add files via upload
    This is an exercise showcasing the use of Boolean Logic to decide whether someone qualifies for car insurance.

---
+++ InsuranceCheck/InsuranceCheck/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace InsuranceCheck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Car Insurance Approval Program");
+            Console.WriteLine();
+
+            //Applicant age input
+            Console.WriteLine("What is your age?");
+            int age = int.Parse(Console.ReadLine());
+
+            //Applicant's DUI input
+            Console.WriteLine("Have you ever had a DUI? (true/false)");
+            bool hasDUI = bool.Parse(Console.ReadLine());
+
+            //Speeding input
+            Console.WriteLine("How many speeding tickets do you have?");
+            int speedingTickets = int.Parse(Console.ReadLine());
+
+            //Qualifications input
+            bool isQualified = (age > 15) && !hasDUI && (speedingTickets <= 3);
+
+            //Result
+            Console.WriteLine();
+            Console.WriteLine("Qualified for car insurance: " + isQualified);
+
+            Console.ReadLine();
+        }
+    }
+}
