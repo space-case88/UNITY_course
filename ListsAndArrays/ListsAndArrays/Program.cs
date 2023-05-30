9d17b820ed2c79367a77d2852c970c887899d96e
space-case88 <128758348+space-case88@users.noreply.github.com>
2023-05-30 15:34:36 -0700

    Add files via upload
    This is a list/arrays exercise. It is a console app that allows you to choose data from a small list.

---
+++ ListsAndArrays/ListsAndArrays/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+
+    class Program
+    {
+        static void Main()
+        {
+            string[] stringArray = { "Apple", "Banana", "Orange" };
+            int[] intArray = { 1, 2, 3, 4, 5 };
+            List<string> stringList = new List<string> { "Cat", "Dog", "Bird" };
+
+            Console.WriteLine("Select an index for the string array (0-2):");
+            int stringIndex = Convert.ToInt32(Console.ReadLine());
+
+            if (stringIndex >= 0 && stringIndex < stringArray.Length)
+            {
+                Console.WriteLine("Selected index: " + stringIndex);
+                Console.WriteLine("String at index " + stringIndex + ": " + stringArray[stringIndex]);
+            }
+
+            else
+            {
+                Console.WriteLine("Invalid Index.");
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine("Select an index for the string list (0-2):");
+            int intIndex = Convert.ToInt32(Console.ReadLine());
+
+            if (intIndex >= 0 && intIndex < stringList.Count)
+            {
+                Console.WriteLine("Selected index: " + intIndex);
+                Console.WriteLine("Content at index " + intIndex + ": " + stringList[intIndex]);
+            }
+            else
+            {
+                Console.WriteLine("Invalid index. Index doesn't exist.");
+            }
+
+            Console.ReadLine();
+
+
+        }
+    }
+
