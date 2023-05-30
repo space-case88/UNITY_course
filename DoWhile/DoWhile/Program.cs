76b24786122fbee3e91b7f7aec5e8b435d53b76e
space-case88 <128758348+space-case88@users.noreply.github.com>
2023-05-29 17:20:12 -0700

    Add files via upload
    While and Do-While statements

---
+++ DoWhile/DoWhile/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace DoWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("While comparison");
+            WhileComparison();
+
+            Console.WriteLine("\nDo-While comparison:");
+            DoWhileComparison();
+
+            Console.ReadLine();
+
+        }
+
+        static void WhileComparison()
+        {
+            int num = 0;
+
+            while (num < 5)
+            {
+                Console.WriteLine("Number: " + num);
+                num++;
+            }
+        }
+
+        static void DoWhileComparison()
+        {
+            int num = 0;
+
+            do
+            {
+                Console.WriteLine("Number: " + num);
+                num++;
+            } while (num < 5);
+        }
+    }
+}
