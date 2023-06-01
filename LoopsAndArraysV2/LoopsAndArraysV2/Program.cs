b08679dd1218c197723d6ca04929c8de2d770b0f
space-case88 <128758348+space-case88@users.noreply.github.com>
2023-06-01 15:29:35 -0700

    Loops and arrays v2

---
+++ LoopsAndArraysV2/LoopsAndArraysV2/Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace InfiniteLoops
+{
+    class Program
+    {
+        static void Main()
+        {
+            // one-dimensional array of strings
+            string[] array = new string[6] { "I", "can't", "wait", "for", "Diablo", "IV" };
+
+            // ask the user to input some text.
+            Console.Write("Enter some text: ");
+            string userInput = Console.ReadLine();
+
+            // a loop that iterates through each string in the array
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] += userInput;
+            }
+
+            // print each string on the array in a separate line
+            foreach (string item in array)
+            {
+                Console.WriteLine(item);
+            }
+
+            // a fixed infinite loop that will execute properly
+            bool isRunning = true;
+            while (isRunning)
+            {
+                Console.WriteLine("This is an infinite loop. Press any key to stop.");
+                Console.ReadKey();
+                isRunning = false;
+            }
+
+            // comparison loop with '<' operator.
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+
+            // comparison loop with '<=' operator
+            for (int i = 0; i <= array.Length - 1; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+
+            // string with list of unique items
+            List<string> uniqueList = new List<string>() { "Barbarian", "Rogue", "Sorcerer", "Necromancer", "Druid" };
+
+            // ask user for text input
+            Console.Write("Enter text to search in the list: ");
+            string searchText = Console.ReadLine();
+
+            // a loop that iterates through the list and displays the index
+            bool found = false;
+            for (int i = 0; i < uniqueList.Count; i++)
+            {
+                if (string.Equals(uniqueList[i], searchText, StringComparison.OrdinalIgnoreCase))
+                {
+                    Console.WriteLine("Match found at index: " + i);
+                    found = true;
+                    break;
+                }
+            }
+
+            // Tell the user if text is not in the list
+            if (!found)
+            {
+                Console.WriteLine("Text not found in the list.");
+            }
+
+            // list of strings that has at least two identical strings in the list.
+            List<string> duplicateList = new List<string> { "Barbarian", "Thief", "Sorcerer", "Necromancer", "Paladin" };
+
+            // loop that iterates through the loop and displays the indices in the list that contain matching text.
+            Console.Write("Enter text to search in the duplicate list: ");
+            string searchDuplicateText = Console.ReadLine();
+            List<int> matchingIndices = new List<int>();
+
+            for (int i = 0; i < duplicateList.Count; i++)
+            {
+                if (string.Equals(duplicateList[i], searchDuplicateText, StringComparison.OrdinalIgnoreCase))
+                {
+                    matchingIndices.Add(i);
+                }
+            }
+
+            // tell the user that the text is not in the list
+            if (matchingIndices.Count == 0)
+            {
+                Console.WriteLine("Text not found in the list.");
+            }
+            else
+            {
+                foreach (int index in matchingIndices)
+                {
+                    Console.WriteLine("Matching index: " + index);
+                }
+            }
+
+            // a list of strings that has at least two identical strings in the list.
+            List<string> identicalList = new List<string>() { "Barbarian", "Rogue", "Sorcerer", "Necromancer", "Druid" };
+
+            // foreach loop that evaluates each item in the list and displays a message showing the string and whether or not it has already appeared in the list.
+            List<string> seenItems = new List<string>();
+
+            foreach (string item in identicalList)
+            {
+                if (seenItems.Contains(item))
+                {
+                    Console.WriteLine(item + " (Already appeared in the list)");
+                }
+                else
+                {
+                    seenItems.Add(item);
+                    Console.WriteLine(item);
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ StringsArrays2/StringsArrays2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace StringsArrays2
+{
+    class Program
+    {
+        static void Main()
+        {
+            //Define the array of strings
+            string[] stringsArray = new string[7];
+
+            //Ask user for input
+            Console.WriteLine("Enter some text:");
+            string userInput = Console.ReadLine();
+
+            //Add user's text input to the end of each string
+            for (int i = 0; i < stringsArray.Length; i++)
+            {
+                stringsArray[i] = stringsArray[i] + userInput;
+            }
+
+            //Print each string in the array on a seperate line
+            foreach (string str in stringsArray)
+            {
+                Console.WriteLine(str);
+            }
+
+            while (true)
+            {
+                Console.WriteLine("This is an infinite loop. Press any key to exit.");
+                Console.ReadLine();
+                break;  //adding a break to exit the loop
+            }
+
+            int counter1 = 0;
+            while (counter1 < 5)
+            {
+                Console.WriteLine("Loop with < comparison operator. Counter: " + counter1);
+                counter1++;
+            }
+
+            // Infinite loop with <= comparison
+            int counter2 = 0;
+            do
+            {
+                Console.WriteLine("Loop with <= comparison. Counter: " + counter2);
+            } while (counter2 <= 5);
+
+            Console.ReadLine();
+        }
+    }
+}
