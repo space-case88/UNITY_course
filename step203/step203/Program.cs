329c14900edd665ee36c9c37403e319333b346c5
space-case88 <128758348+space-case88@users.noreply.github.com>
2023-06-02 15:19:28 -0700

    TryCatch block

---
+++ step203/step203/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace step203
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<int> numbers = new List<int> { 10, 20, 30, 40, 50 };
+
+            Console.Write("Enter a number to divide each element in the list by: ");
+            string input = Console.ReadLine();
+
+            int divisor;
+            bool isValidNumber = int.TryParse(input, out divisor);
+
+            try
+            {
+                if(isValidNumber)
+                {
+                    for (int i = 0; i < numbers.Count; i++)
+                    {
+                        int result = numbers[i] / divisor;
+                        Console.WriteLine($"Resilt of division: {result}");
+                    }
+                }
+                else
+                {
+                    throw new ArgumentException("Invalid number entered.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+
+            Console.WriteLine("Pogram execution continued after the try/catch block.");
+
+            Console.ReadLine();
+        }
+    }
+}
