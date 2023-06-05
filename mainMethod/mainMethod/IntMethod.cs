4d6f0dbe7326e84750d803be8c8448946177a59b
space-case88 <128758348+space-case88@users.noreply.github.com>
2023-06-05 16:45:11 -0700

    ClassMethods

---
+++ mainMethod/mainMethod/IntMethod.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace mainMethod
+{
+    class IntMethod
+    {
+        public int Calculate(int number)
+        {
+            int result = (int)(number * 2);
+            return result;
+        }
+
+        public int Calculate(decimal number)
+        {
+            int result = (int)(number + 3);
+            return result;
+        }
+        public int Calculate(string number)
+        {
+            int parsedNumber;
+            bool success = int.TryParse(number, out parsedNumber);
+
+            if (success)
+            {
+                int result = parsedNumber - 5;
+                return result;
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Unable to convert the string to an integer.");
+                return 0;
+            }
+        }
+    }
+}
---
+++ mainMethod/mainMethod/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace mainMethod
+{
+    class Program
+    {
+        static void Main()
+        {
+            IntMethod calculator = new IntMethod();
+
+            int result1 = calculator.Calculate(10);
+            Console.WriteLine("Result 1: " + result1);
+
+            int result2 = calculator.Calculate(7.5m);
+            Console.WriteLine("Result 2: " + result2);
+
+            int result3 = calculator.Calculate("15");
+            Console.WriteLine("Result 3: " + result3);
+
+            Console.ReadLine();
+        }
+    }
+}
