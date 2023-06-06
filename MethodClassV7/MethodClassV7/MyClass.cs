4727a19e4549aaa49e575e89bb39c36331e13f94
space-case88 <128758348+space-case88@users.noreply.github.com>
2023-06-06 16:26:18 -0700

    step224

---
+++ MethodClassV7/MethodClassV7/MyClass.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MethodClassV7
+{
+    class MyClass
+    {
+        public int DividedByTwo(int number)
+        {
+            return number / 2;
+        }
+
+        public void MethodWithOutputParams(int input, out int output1, out int output2)
+        {
+            output1 = input * 2;
+            output2 = input * 3;
+        }
+
+        public void OverloadedMethod(int number)
+        {
+            Console.WriteLine("OverloadedMethod(int): " + (number * 2));
+        }
+
+        public void OverloadedMethod(int number, string message)
+        {
+            Console.WriteLine("OverloadedMethod(int, string): " + message + (number * 3));
+        }
+    }
+
+    static class StaticClass
+    {
+        public static void StaticMethod()
+        {
+            Console.WriteLine("This is a static method.");
+        }
+    }
+}
---
+++ MethodClassV7/MethodClassV7/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace MethodClassV7
+{
+    class Program
+    {
+        static void Main()
+        {
+            // instantiate class
+            MyClass myObject = new MyClass();
+
+            // User input
+            Console.WriteLine("Enter a number: ");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+
+            // call the method and display output
+            int result = myObject.DividedByTwo(userInput);
+            Console.WriteLine("Result: " + result);
+
+            // call the method with output parameters
+            int outputParam1, outputParam2;
+            myObject.MethodWithOutputParams(userInput, out outputParam1, out outputParam2);
+            Console.WriteLine("Output Parameter 1: " + outputParam1);
+            Console.WriteLine("Output Parameter 2: " + outputParam2);
+
+            // Overloaded method
+            myObject.OverloadedMethod(userInput);
+            myObject.OverloadedMethod(userInput, userInput.ToString());
+
+            // Static class
+            StaticClass.StaticMethod();
+        }
+    }
+}
