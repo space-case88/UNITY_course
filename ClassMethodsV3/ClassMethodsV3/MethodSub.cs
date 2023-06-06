3b06c1ff5902b1390381713de50b9f250ee8b05c
space-case88 <128758348+space-case88@users.noreply.github.com>
2023-06-05 17:29:48 -0700

    MathOpClass&Methods

---
+++ ClassMethodsV3/ClassMethodsV3/MethodSub.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMethodsV3
+{
+    class MethodSub
+    {
+        public int PerformMathOperation(int num1, int num2 = 0)
+        {
+            int result = num1 - num2;
+            return result;
+        }
+    }
+}
---
+++ ClassMethodsV3/ClassMethodsV3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ClassMethodsV3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MethodSub methodSub = new MethodSub();
+
+            Console.Write("Enter the first number: ");
+            int number1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter the second number (optional): ");
+            string input = Console.ReadLine();
+
+            int number2 = 0;
+            if (!string.IsNullOrEmpty(input))
+            {
+                number2 = int.Parse(input);
+            }
+
+            int result = methodSub.PerformMathOperation(number1, number2);
+            Console.WriteLine("The result of the math operation is: " + result);
+        }
+    }
+}
