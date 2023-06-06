78f0d01a88199ca0634857f0e849630cf6156100
space-case88 <128758348+space-case88@users.noreply.github.com>
2023-06-06 15:02:55 -0700

    Step 220

---
+++ MethodClassV6/MethodClassV6/MyClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MethodClassV6
+{
+    class MyClass
+    {
+        public void MyMethod(int firstNumber, int secondNumber)
+        {
+            int result = firstNumber * 2;
+
+            Console.WriteLine("Second number: " + secondNumber);
+        }
+    }
+}
---
+++ MethodClassV6/MethodClassV6/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MethodClassV6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass myObject = new MyClass();
+            myObject.MyMethod(5, 10);
+            myObject.MyMethod(firstNumber: 7, secondNumber: 15);
+
+            Console.ReadLine(); 
+        }
+    }
+}
