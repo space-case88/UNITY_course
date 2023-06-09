943037b2f8395a8ea4c0cf73e78f3e7142889b02
space-case88 <128758348+space-case88@users.noreply.github.com>
2023-06-09 15:02:59 -0700

    fixed not equal

--- Overload/Overload/Employee.cs
+++ Overload/Overload/Employee.cs
@@ -11,32 +11,12 @@ namespace Overload
 
         public static bool operator ==(Employee emp1, Employee emp2)
         {
-            if (ReferenceEquals(emp1, emp2))
-                return true;
-
-            if (ReferenceEquals(emp1, null) || ReferenceEquals(emp2, null))
-                return false;
-
             return emp1.Id == emp2.Id;
         }
 
         public static bool operator !=(Employee emp1, Employee emp2)
         {
-            return !(emp1 == emp2);
-        }
-
-        public override bool Equals(object obj)
-        {
-            if (obj == null || GetType() != obj.GetType())
-                return false;
-
-            Employee other = (Employee)obj;
-            return Id == other.Id;
-        }
-
-        public override int GetHashCode()
-        {
-            return Id.GetHashCode();
+            return emp1.Id != emp2.Id;
         }
     }
 }
