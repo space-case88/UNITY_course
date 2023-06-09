c8e41f953b9b421cc4025f2be766daf2dc6f0d88
space-case88 <128758348+space-case88@users.noreply.github.com>
2023-06-09 12:03:28 -0700

    step 253

---
+++ Overload/Overload/Employee.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Overload
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public static bool operator ==(Employee emp1, Employee emp2)
+        {
+            if (ReferenceEquals(emp1, emp2))
+                return true;
+
+            if (ReferenceEquals(emp1, null) || ReferenceEquals(emp2, null))
+                return false;
+
+            return emp1.Id == emp2.Id;
+        }
+
+        public static bool operator !=(Employee emp1, Employee emp2)
+        {
+            return !(emp1 == emp2);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || GetType() != obj.GetType())
+                return false;
+
+            Employee other = (Employee)obj;
+            return Id == other.Id;
+        }
+
+        public override int GetHashCode()
+        {
+            return Id.GetHashCode();
+        }
+    }
+}
---
+++ Overload/Overload/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Overload
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp1 = new Employee { Id = 1, Name = "John" };
+            Employee emp2 = new Employee { Id = 2, Name = "Jane" };
+            Employee emp3 = new Employee { Id = 1, Name = "John" };
+
+            Console.WriteLine(emp1 == emp2);  // False
+            Console.WriteLine(emp1 == emp3);  // True
+
+            Console.WriteLine(emp1 != emp2);  // True
+            Console.WriteLine(emp1 != emp3);  // False
+        }
+    }
+}
